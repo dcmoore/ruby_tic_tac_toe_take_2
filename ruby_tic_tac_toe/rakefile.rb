d043ef75e30dc30d3e37accefcb79422cc669152
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-08-03 14:56:20 -0500

    Fixed up the rakefile that shows test coverage with the 'rake spec:rcov' command.

--- ruby_tic_tac_toe/rakefile.rb
+++ ruby_tic_tac_toe/rakefile.rb
@@ -3,7 +3,8 @@ require 'rspec/core/rake_task'
 namespace :spec do
   desc "Run specs with RCov"
   RSpec::Core::RakeTask.new('rcov') do |spec|
-    spec.pattern = FileList['/Users/dcmoore/Projects/ruby_tic_tac_toe/ruby_tic_tac_toe/spec/**/*_spec.rb']
+    spec.pattern = FileList['spec/**/*_spec.rb']
     spec.rcov = true
+    spec.rcov_opts = "--exclude osx\/objc,gems\/,spec\/,features\/"
   end
 end
