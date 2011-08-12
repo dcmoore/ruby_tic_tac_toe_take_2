91065a3b026dcb13ff5f66e9a7e3eaf2d6f8ba58
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-08-12 09:09:06 -0500

    Created Rails skeleton.

---
+++ ruby_tic_tac_toe/rails-3.0.9/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails309::Application.config.secret_token = 'df61f719bcd06118ce5c94e1bf7de8ce0bf4b03409570976ce2cc95f6477e851b06190e3e1bb0ac5a249f6e1380c626df134f24f5652c9873d84c6499a35ba33'
